8c3585827fa38eea1605d56946b43ea5d21c54db
GutoAS <agostinhosoberano2@gmail.com>
2023-01-02 18:33:14 +0200

    intial commit

---
+++ 2048/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./main.scss";
+import "./styles.scss";
+
+const App = () => {
+  return <h1>2048</h1>;
+};
+
+export default App;
---
+++ 2048/src/components/Board.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+
+const Board = () => {
+  const [board, setBoard] = useState(new Board());
+
+  return <div>Board</div>;
+};
+
+export default Board;
